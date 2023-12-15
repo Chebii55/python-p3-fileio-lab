01411f6638b270a4d2d74088465ba092e8fea6e6
Sheila Chebii <shillerchier055@gmail.com>
2023-12-15 15:37:33 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    full_name = str(file_name)+ '.txt'
+    with open(full_name, mode='w', encoding='utf-8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    full_name=str(file_name)+'.txt'
+    with open(full_name, mode='a', encoding='utf-8') as file:
+         file.write(append_content)
 
 def read_file(file_name):
-    pass
+    full_name = str(file_name) + '.txt'
+    with open(full_name, encoding='utf-8') as text_file:
+        return text_file.read()
+    
